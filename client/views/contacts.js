ace56fd6993f2cdc1a5db228df46da3c5503c9d7
Philipp Muens <raute1337@gmx.de>
2015-10-03 22:40:42 +0200

    Add search contacts functionality
    Add search contacts functionality so that users are
    able to search through their contacts.

--- client/views/contacts.js
+++ client/views/contacts.js
@@ -7,11 +7,20 @@ Template.contacts.events({
     if (confirm('Do you really want to remove this contact?')) {
       Contacts.remove({ _id: this._id });
     }
-  }
+  },
+
+  'keyup [data-action=search]': _.debounce(function(event, template) {
+    event.preventDefault();
+    Session.set('searchQuery', template.find('[data-action=search]').value);
+  }, 300)
 });
 
 Template.contacts.helpers({
   contacts: () => {
+    if (Session.get('searchQuery') !== undefined && Session.get('searchQuery').length !== 0) {
+      const query = Session.get('searchQuery');
+      return Contacts.find({ $or: [ { name: new RegExp(query, 'i') }, { position: new RegExp(query, 'i') }, { company: new RegExp(query, 'i') } ] }, { sort: { name: 1 } });
+    }
     return Contacts.find({}, { sort: { name: 1 } });
   }
 });
